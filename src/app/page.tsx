3dfe7305df73667ab28b9e3a64bfe9263953b8da
shakti sumant das <shakti.20k@gmail.com>
2024-06-26 23:36:43 +0530

    add button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <h1>Welcome to Pizza shop</h1>
+      <Button variant="outline">Button</Button>
     </main>
   );
 }
