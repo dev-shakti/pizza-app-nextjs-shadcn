22a9f5066435c09e2c048d26c06a9ff0b90449d9
shakti sumant das <shakti.20k@gmail.com>
2024-06-26 23:53:52 +0530

    configure font family

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="">
       <h1>Welcome to Pizza shop</h1>
-      <Button>Button</Button>
+      <Button >Button</Button>
     </main>
   );
 }
